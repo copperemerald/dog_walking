d1137dbee25a2a00d8c3f84725844788e9b4bc19
Guy Borthwick <guyborthwick@yahoo.com>
2023-11-07 19:08:06 +0000

    added dogs new view

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,12 @@
 class Dog < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  # adding info that has a picture attached
+  has_one_attached :photo
+
+  # Validations for Dog:
+
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :description, presence: true
 end
