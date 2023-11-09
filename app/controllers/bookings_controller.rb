621810a07ddebb021493f7d727a0ce8398790208
Clint Ziervogel <photon01.joins@icloud.com>
2023-11-09 22:04:00 +0200

    bookings controller done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,15 +2,22 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
+    @dog = Dog.find(params[:dog_id])
+
   end
 
   def create
     @booking = Booking.new(booking_params)
-    @booking.dog =
+    @dog = Dog.find(params[:dog_id])
+    @booking.dog = @dog
     @booking.user = current_user
+    puts @booking.valid?
     if @booking.save
-      redirect_to @booking, notice 'Congrats, enjoy the walk!!'
+
+      # redirect_to booking_path(@booking), notice: 'Congrats, enjoy the walk!!'
+      redirect_to root_path, notice: 'Congrats, enjoy the walk!!'
     else
+      puts @booking.errors.messages
       render :new, status: :unprocessable_entity
     end
   end
@@ -19,16 +26,15 @@ class BookingsController < ApplicationController
     @booking = Booking.all
   end
 
-  def book
-    @dog = Dog.find(params[:id])
-    @dog.book
-    redirect_to @dog
+  # def book
+  #   @dog = Dog.find(params[:id])
+  #   @dog.book
+  #   redirect_to @dog
+  # end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :comment, :dog_id, :user_id)
   end
-
-private
-
-def booking_params
-  params.require(:booking).permit(:date, :comment, :dog_id)
-end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   root to: "pages#home"
   # Defines the root path route ("/")
-  resources :dogs, only: [:index, :show, :new, :create]
+  resources :dogs, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:new, :create, :show]
+  end
+
 
   # root "articles#index"
 end
