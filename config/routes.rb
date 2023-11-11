fc61c0077a605e3fb3b7e0dff156f6b81d49c6f6
Guy Borthwick <guyborthwick@yahoo.com>
2023-11-11 13:34:00 +0000

    Updated routes to direct to dogs/index as default home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  root to: "pages#home"
+  root to: "dogs#index"
   # Defines the root path route ("/")
   resources :dogs, only: [:index, :show, :new, :create] do
     resources :bookings, only: [:new, :create, :show, :index]
